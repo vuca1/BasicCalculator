841d84204e6249665adde199e74cc6a8a0e17132
vuca1 <malihavita@gmail.com>
2025-04-28 13:19:33 +0200

    added erase and removed bugs with floats in delete_last

--- app.py
+++ app.py
@@ -26,6 +26,7 @@ def main():
     # remove the entry_box and put a new value to it
     def change_label(value):
         global equation
+        equation = value
         value = str(value)
         entry_box.delete(0, tk.END)
         entry_box.insert(0, value)
@@ -33,6 +34,7 @@ def main():
     # delete last character from 'entry_box'
     def delete_last():
         global equation
+        equation = str(equation)
         equation = equation[:-1]
         change_label(equation)
 
@@ -50,15 +52,15 @@ def main():
 
     # grid
     buttons = [
-            ("1", 1, 0),
-            ("2", 1, 1),
-            ("3", 1, 2),
-            ("4", 2, 0),
-            ("5", 2, 1),
-            ("6", 2, 2),
-            ("7", 3, 0),
-            ("8", 3, 1),
-            ("9", 3, 2)
+            (" 1 ", 1, 0),
+            (" 2 ", 1, 1),
+            (" 3 ", 1, 2),
+            (" 4 ", 2, 0),
+            (" 5 ", 2, 1),
+            (" 6 ", 2, 2),
+            (" 7 ", 3, 0),
+            (" 8 ", 3, 1),
+            (" 9 ", 3, 2)
         ]
 
     # number buttons - creates all buttons from 1 to 10(9)
@@ -66,33 +68,41 @@ def main():
             num_button = tk.Button(window, text=text, command=lambda num=text: add_to_equation(num))
             num_button.grid(row=row, column=col, padx=3, pady=3)
 
+    # zero button
+    zero_button = tk.Button(window, text=" 0 ", command=lambda: add_to_equation("0"))
+    zero_button.grid(row=4, column=1, padx=3, pady=3)
+
     # minus button
-    minus_button = tk.Button(window, text="-", command=lambda: add_to_equation("-"))
+    minus_button = tk.Button(window, text=" - ", command=lambda: add_to_equation("-"))
     minus_button.grid(row=2, column=3, padx=3, pady=3)
 
     # plus button
-    plus_button = tk.Button(window, text="+", command=lambda: add_to_equation("+"))
+    plus_button = tk.Button(window, text=" + ", command=lambda: add_to_equation("+"))
     plus_button.grid(row=1, column=3, padx=3, pady=3)
 
     # divide button
-    divide_button = tk.Button(window, text="/", command=lambda: add_to_equation("/"))
+    divide_button = tk.Button(window, text=" / ", command=lambda: add_to_equation("/"))
     divide_button.grid(row=3, column=3, padx=3, pady=3)
 
     # multiply button
-    multiply_button = tk.Button(window, text="*", command=lambda: add_to_equation("*"))
+    multiply_button = tk.Button(window, text=" * ", command=lambda: add_to_equation("*"))
     multiply_button.grid(row=4, column=3, padx=3, pady=3)
 
     # equals button
-    equals_button = tk.Button(window, text="=", command=calculate)
-    equals_button.grid(row=4, column=2, padx=3, pady=3)
+    equals_button = tk.Button(window, text="  =  ", command=calculate)
+    equals_button.grid(row=5, column=3, padx=3, pady=3)
 
     # back / delete button
-    back_button = tk.Button(window, text="back", command=delete_last)
-    back_button.grid(row=4, column=1, padx=3, pady=3)
+    back_button = tk.Button(window, text="Back", command=delete_last)
+    back_button.grid(row=5, column=1, padx=3, pady=3)
+
+    # erase button
+    erase_button = tk.Button(window, text="Erase", command=lambda: change_label(""))
+    erase_button.grid(row=5, column=2, padx=3, pady=3)
 
     # decimal button
-    decimal_button = tk.Button(window, text=".", command=lambda: add_to_equation("."))
-    decimal_button.grid(row=4, column=0, padx=3, pady=3)
+    decimal_button = tk.Button(window, text=" . ", command=lambda: add_to_equation("."))
+    decimal_button.grid(row=4, column=2, padx=3, pady=3)
 
     window.mainloop()
 
